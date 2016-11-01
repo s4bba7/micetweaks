19b56e34ebd89cb34d66037421c6ea353f97d8f5
s4bba7 <s4bba7@gmail.com>
2016-11-01 10:53:34 +0100

    Fixed leaking BufferedReader stream.

--- micetweaks/src/com/micetweaks/HotPlug.java
+++ micetweaks/src/com/micetweaks/HotPlug.java
@@ -12,56 +12,52 @@ import java.util.Map;
  * <p>
  * author Łukasz 's4bba7' Gąsiorowski
  */
-public class HotPlug {
+class HotPlug {
 	/**
 	 * Detects plugged in devices and adds or removes them from Assets.DEVICES_LIST.
 	 *
 	 * @param removeFlag device might be plugged off so mark it TRUE to additionally remove that device from list.
 	 */
-	public static void detectUsbDevices(boolean removeFlag) {
+	static void detectUsbDevices(boolean removeFlag) {
 		String buff;
 		Process p = xinputExecute();
 		HashSet<String> devNameBuff = new HashSet<>();
 
-		if (p != null) {
-			try {
-				BufferedReader in = new BufferedReader(new InputStreamReader(p.getInputStream()));
+		if (p != null) try (BufferedReader in = new BufferedReader(new InputStreamReader(p.getInputStream()))) {
+			while ((buff = in.readLine()) != null) {
+				String[] dev = buff.split("\t");
 
-				while ((buff = in.readLine()) != null) {
-					String[] dev = buff.split("\t");
+				if (dev[2].contains("slave  pointer")) {
+					String devName = dev[0].substring(6).trim();
 
-					if (dev[2].contains("slave  pointer")) {
-						String devName = dev[0].substring(6).trim();
+					if (!devName.equalsIgnoreCase("Virtual core XTEST pointer")) {
+						int id = Integer.parseInt(dev[1].substring(3));
+						devNameBuff.add(devName);
 
-						if (!devName.equalsIgnoreCase("Virtual core XTEST pointer")) {
-							int id = Integer.parseInt(dev[1].substring(3));
-							devNameBuff.add(devName);
-
-							// If device exist in hashmap just add another devID into it.
-							if (Assets.DEVICES_LIST.containsKey(devName)) {
-								DeviceProps devID = Assets.DEVICES_LIST.get(devName);
-								devID.addId(id);
-							} else {
-								DeviceProps devID = new DeviceProps(id);
-								Assets.DEVICES_LIST.put(devName, devID);
-							}
+						// If device exist in hashmap just add another devID into it.
+						if (Assets.DEVICES_LIST.containsKey(devName)) {
+							DeviceProps devID = Assets.DEVICES_LIST.get(devName);
+							devID.addId(id);
+						} else {
+							DeviceProps devID = new DeviceProps(id);
+							Assets.DEVICES_LIST.put(devName, devID);
 						}
-						// When there's no more pointers just break.
-					} else if (dev[2].contains("keyboard")) break;
-				}
-
-				// Check if any device is plugged off. If so remove it from list.
-				if (removeFlag) {
-					Iterator<Map.Entry<String, DeviceProps>> i = Assets.DEVICES_LIST.entrySet().iterator();
-					for (; i.hasNext(); ) {
-						Map.Entry<String, DeviceProps> e = i.next();
-						if (!devNameBuff.contains(e.getKey())) i.remove();
 					}
-				}
-			} catch (IOException e) {
-				Log.write(e.getMessage());
-				e.printStackTrace();
+					// When there's no more pointers just break.
+				} else if (dev[2].contains("keyboard")) break;
 			}
+
+			// Check if any device is plugged off. If so remove it from list.
+			if (removeFlag) {
+				for (Iterator<Map.Entry<String, DeviceProps>> i = Assets.DEVICES_LIST.entrySet().iterator(); i
+						.hasNext(); ) {
+					Map.Entry<String, DeviceProps> e = i.next();
+					if (!devNameBuff.contains(e.getKey())) i.remove();
+				}
+			}
+		} catch (IOException e) {
+			Log.write(e.getMessage());
+			e.printStackTrace();
 		}
 
 	}
