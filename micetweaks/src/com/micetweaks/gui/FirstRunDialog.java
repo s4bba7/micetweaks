e32016ee269cb2a679149b247416199b413c116e
s4bba7 <s4bba7@gmail.com>
2017-04-16 12:57:43 +0200

    Rearranged and optimized class.

--- micetweaks/src/com/micetweaks/gui/FirstRunDialog.java
+++ micetweaks/src/com/micetweaks/gui/FirstRunDialog.java
@@ -17,39 +17,59 @@ import javafx.stage.StageStyle;
  * @author Łukasz 's4bba7' Gąsiorowski
  */
 public class FirstRunDialog extends Stage {
-	private Label    title    = new Label();
-	private TextArea text     = new TextArea();
-	private Button   okButton = new Button("OK");
+	private final String   greetingsText =
+			"How-to:\n" + "1. To hide or show program's window just click on the tray icon.\n"
+					+ "2. To save configuration click \"save config\" button.\n"
+					+ "3. To adjust mouse/touchpad speed or deceleration press and hold scrollbar, then slide "
+					+ "it to the left to decrease property or to the right to increase it. When you "
+					+ "release the button the property will be applied immediately." + "\n";
+	private       Label    title         = new Label();
+	private       TextArea text          = new TextArea();
+	private       Button   okButton      = new Button("OK");
 	private Scene scene;
 	private BorderPane pane = new BorderPane();
 
 	public FirstRunDialog() {
-		setTitle("Micetweaks");
-		getIcons().add(new Image(Assets.ICON));
-		setAlwaysOnTop(true);
-		setResizable(false);
-		initStyle(StageStyle.UTILITY);
+		setDialogProperties();
+		setGreetingsText();
+		setEventHandlers();
+		setLayout();
+		setScene();
+	}
 
-		title.setText("Thank you for using this software!");
-		title.setId("firstRunTitle");
-
-		text.setId("firstRunText");
-		text.setEditable(false);
-		text.setText("How-to:\n1. To hide or show program's window just click on the tray icon.\n"
-				             + "2. To save configuration click \"save config\" button.\n"
-				             + "3. To adjust mouse/touchpad speed or deceleration press and hold scrollbar, then slide "
-				             + "it to the left to decrease property or to the right to increase it. When you "
-				             + "release the button the property will be applied immediately." + "\n");
+	private void setEventHandlers() {
 		okButton.setOnMouseClicked(e -> { close(); });
+	}
 
+	private void setScene() {
+		scene = new Scene(pane);
+		scene.getStylesheets().add(Assets.CSS_PATH);
+		setScene(scene);
+	}
+
+	private void setLayout() {
 		pane.setTop(title);
 		pane.setCenter(text);
 		pane.setBottom(okButton);
 		BorderPane.setAlignment(okButton, Pos.CENTER);
 		BorderPane.setAlignment(title, Pos.CENTER);
-		scene = new Scene(pane);
-		scene.getStylesheets().add(Assets.CSS_PATH);
-		setScene(scene);
+	}
+
+	private void setDialogProperties() {
+		setTitle("Micetweaks");
+		getIcons().add(new Image(Assets.ICON));
+		setAlwaysOnTop(true);
+		setResizable(false);
+		initStyle(StageStyle.UTILITY);
+	}
+
+	private void setGreetingsText() {
+		title.setId("firstRunTitle");
+		title.setText("Thank you for using this software!");
+
+		text.setId("firstRunText");
+		text.setEditable(false);
+		text.setText(greetingsText);
 	}
 
 }
