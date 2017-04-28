39babffb69d814b8ac3837416a9fa9f3f9d173fb
s4bba7 <s4bba7@gmail.com>
2017-04-28 20:12:46 +0200

    Added theme changer.

---
+++ micetweaks/src/com/micetweaks/gui/device/components/IconsThemeItem.java
@@ -0,0 +1,41 @@
+package com.micetweaks.gui.device.components;
+
+import com.micetweaks.gui.Theme;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.control.MenuItem;
+import javafx.stage.Stage;
+
+import java.awt.*;
+
+/**
+ * Created on 28/04/17.
+ *
+ * @author Łukasz 's4bba7' Gąsiorowski
+ */
+public class IconsThemeItem extends MenuItem implements EventHandler<ActionEvent> {
+	private String   option;
+	private TrayIcon trayIcon;
+	private Stage    stage;
+
+	public IconsThemeItem(String text, TrayIcon trayIcon, Stage stage) {
+		option = text;
+		setText("Theme: " + text + " icons");
+		this.trayIcon = trayIcon;
+		this.stage = stage;
+		setOnAction(this);
+	}
+
+	@Override public void handle(ActionEvent event) {
+		switch (option.toLowerCase()) {
+			case "white":
+				Theme.setWhiteIcons(trayIcon);
+				Theme.setWhiteIcons(stage);
+				break;
+			case "black":
+				Theme.setBlackIcons(trayIcon);
+				Theme.setBlackIcons(stage);
+				break;
+		}
+	}
+}
